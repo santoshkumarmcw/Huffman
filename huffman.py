191c41ea9d3c4bfbee1500b398a1100928c4257a
Santoshkumar <santoshkumar@multicorewareinc.com>
2020-11-01 10:02:22 +0530

    updated huffman.py

--- huffman.py
+++ huffman.py
@@ -46,12 +46,37 @@ def encode(input_file, output_file):
 		data_str += mask[len(str(bin(ord(length_str[1]))[2:]))] + str(bin(ord(length_str[1]))[2:])
 		data_str += mask[len(str(bin(ord(length_str[2]))[2:]))] + str(bin(ord(length_str[2]))[2:])
 
+	if len(dic) >= 128 and len(dic) < 256:
+		num = 0
+		data_str += mask[len(str(bin(8)[2:]))] + str(bin(8)[2:])
+	elif len(dic) >= 64 and len(dic) < 128:
+		num = 1
+		data_str += mask[len(str(bin(7)[2:]))] + str(bin(7)[2:])
+	elif len(dic) >= 32 and len(dic) < 64:
+		num = 2
+		data_str += mask[len(str(bin(6)[2:]))] + str(bin(6)[2:])
+	elif len(dic) >= 16 and len(dic) < 32:
+		num = 3
+		data_str += mask[len(str(bin(5)[2:]))] + str(bin(5)[2:])
+	elif len(dic) >= 8 and len(dic) < 16:
+		num = 4
+		data_str += mask[len(str(bin(4)[2:]))] + str(bin(4)[2:])
+	elif len(dic) >= 4 and len(dic) < 8:
+		num = 5
+		data_str += mask[len(str(bin(3)[2:]))] + str(bin(3)[2:])
+	elif len(dic) >= 2 and len(dic) < 4:
+		num = 6
+		data_str += mask[len(str(bin(2)[2:]))] + str(bin(2)[2:])
+	elif len(dic) == 1:
+		num = 7
+		data_str += mask[len(str(bin(1)[2:]))] + str(bin(1)[2:])
+	
 	for i in range(len(dic)):
 		data_str += mask[len(str(bin(ord(dd[i]))[2:]))] + str(bin(ord(dd[i]))[2:])
-		data_str += mask[len(str(bin(dic[dd[i]])[2:])) + 1] + str(bin(dic[dd[i]])[2:])
+		data_str += mask[len(str(bin(dic[dd[i]])[2:])) + num] + str(bin(dic[dd[i]])[2:])
 
 	for i in range(len(data)):
-		data_str += mask[len(str(bin(dic[data[i]])[2:])) + 1] + str(bin(dic[data[i]])[2:])
+		data_str += mask[len(str(bin(dic[data[i]])[2:])) + num] + str(bin(dic[data[i]])[2:])
 
 	a = bitarray(data_str)
 	with open(output_file, 'wb') as f:
@@ -79,15 +104,16 @@ def decode(input_file, output_file):
 	length = 0 
 	for i in range(3):
 		length = length*10 + int(chr(int(str_data[i*8:i*8+8],2)))
-	print(length)
 	str_data = str_data[24:]
+	offset = int(str_data[:8],2)
+	str_data = str_data[8:]
 	for i in range(length):
-		dic[int(str_data[i*15+8:i*15+15],2)] = chr(int(str_data[i*15:i*15+8],2))
-	str_data = str_data[length*15:]
+		dic[int(str_data[i*(8+offset)+8:i*(8+offset)+(8+offset)],2)] = chr(int(str_data[i*(8+offset):i*(8+offset)+8],2))
+	str_data = str_data[length*(8+offset):]
 	i = 0
-	num = len(str_data)//7
+	num = len(str_data)//offset
 	for i in range(num):
-		data += dic[int(str_data[i*7:i*7+7],2)] 
+		data += dic[int(str_data[i*offset:i*offset+offset],2)] 
 
 	f = open(output_file,"w")
 	f.write(data)
